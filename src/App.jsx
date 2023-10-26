e6a029c9c2114597dcadf751a9790388bd4c981b
FatemiSolution <shabbir.high@gmail.com>
2023-10-26 17:21:20 +0500

    added css to the file

--- src/App.jsx
+++ src/App.jsx
@@ -46,15 +46,15 @@ localStorage.setItem('todos', JSON.stringify(todos))
 
   return (
     <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
-    <div>
-      <div>
-        <h1>
+    <div className="bg-[#172842] min-h-screen py-8 " >
+      <div className='w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white'>
+        <h1 className="text-2xl font-bold text-center mb-8 mt-2">
           Manage Your TODO
         </h1>
-        <div>
+        <div className="mb-4 text-black">
           <TodoForm/>
         </div>
-        <div>
+        <div className="flex flex-wrap gap-y-3" >
           {todos.map((todo)=>(
             <div key={todo.id} className='w-full'>
               <TodoItem todo={todo}/>
