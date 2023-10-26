2d11e3235a744da3a10df91931cc2d32d384c6d3
FatemiSolution <shabbir.high@gmail.com>
2023-10-26 16:00:08 +0500

    added functionality for the save and getting item from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
 import { TodoProvider } from './contexts'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
   // this todos is an array of multiple todos
@@ -21,6 +22,21 @@ const toggleComplete = (id)=>{
   // in this first we find the prev Todo with same id and then we changed the completed by using turnary operator first we paste all the value of prevTodo and then reversed the reversed the completed option
   setTodos((prev)=>prev.map((prevTodo)=>(prevTodo.id === id? {...prevTodo, completed: !prevTodo.completed}:prevTodo)))
 }
+// this will run when the appliication wll first start 
+useEffect(() => {
+  // this will get the todo from the local Storage
+ const todos = JSON.parse(localStorage.getItem('todos'))
+// this will set the todos once we get it from the local Storage
+ if(todos && todos.length> 0){
+  setTodos(todos)
+ }
+}, [])
+// this will run when ever the todo will be updated 
+useEffect(() => {
+  // localStorage requires string value 
+localStorage.setItem('todos', JSON.stringify(todos))
+}, [todos])
+
   return (
     <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
     <div>
